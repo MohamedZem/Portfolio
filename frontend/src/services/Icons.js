79a73991b5dfb5e2c39a219e045e4347a91e369f
Zemouchi Mohamed <zemouchi.moh@gmail.com>
2026-06-26 15:00:49 +0200

    file icons

---
+++ frontend/src/services/Icons.js
@@ -0,0 +1,74 @@
+import {
+  FaHtml5,
+  FaCss3Alt,
+  FaJs,
+  FaReact,
+  FaNodeJs,
+  FaGitAlt,
+  FaGithub,
+  FaUniversalAccess,
+  FaTasks,
+  FaUpload,
+  FaLock,
+  FaImage,
+  FaSearch
+} from "react-icons/fa";
+
+import {
+  SiSass,
+  SiMongodb,
+  SiExpress,
+  SiJsonwebtokens,
+  SiMongoose,
+  SiLighthouse,
+  SiFigma,
+  SiNotion,
+  SiVite,
+  SiScrumalliance
+} from "react-icons/si";
+
+import {
+  MdApi
+} from "react-icons/md";
+
+import {
+  RiKanbanView
+} from "react-icons/ri";
+
+export const Icons = {
+ HTML5: FaHtml5,
+  CSS3: FaCss3Alt,
+  JavaScript: FaJs,
+  React: FaReact,
+  "React Router": FaReact,
+  Sass: SiSass,
+
+  "Node.js": FaNodeJs,
+  Express: SiExpress,
+  MongoDB: SiMongodb,
+  Mongoose: SiMongoose,
+  JWT: SiJsonwebtokens,
+
+  Git: FaGitAlt,
+  GitHub: FaGithub,
+
+  Lighthouse: SiLighthouse,
+
+  Figma: SiFigma,
+  Notion: SiNotion,
+  Vite: SiVite,
+
+  Scrum: SiScrumalliance,
+  Agile: SiScrumalliance,
+
+  Kanban: RiKanbanView,
+
+  "API REST": MdApi,
+
+  Bcrypt: FaLock,
+  Multer: FaUpload,
+  Sharp: FaImage,
+
+  SEO: FaSearch,
+  Accessibilité: FaUniversalAccess,
+};
