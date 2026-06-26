1100d1a11320031ec344f7ce425a859942cd49b5
Zemouchi Mohamed <zemouchi.moh@gmail.com>
2026-06-26 20:36:16 +0200

    add cache

--- backend/controllers/project.controller.js
+++ backend/controllers/project.controller.js
@@ -13,3 +13,31 @@ exports.getAllProjects = async (req, res) => {
     });
   }
 };
+
+let cachedProjects = null;
+let cacheDate = null;
+
+const CACHE_DURATION = 1000 * 60 * 30;
+
+exports.getProjects = async (req, res) => {
+  try {
+    const now = Date.now();
+
+    if (cachedProjects && cacheDate && now - cacheDate < CACHE_DURATION) {
+      return res.status(200).json(cachedProjects);
+    }
+
+    const projects = await getGithubPortfolioProjects();
+
+    cachedProjects = projects;
+    cacheDate = now;
+
+    return res.status(200).json(projects);
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      message: "Erreur lors de la récupération des projets.",
+    });
+  }
+};
