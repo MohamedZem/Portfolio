dfd02d0d2e2e7c887737c73f44ddf43cda6e9eeb
Zemouchi Mohamed <zemouchi.moh@gmail.com>
2026-06-28 19:23:02 +0200

    remove useEffect

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import { FaGithub, FaLinkedin, FaEnvelope, FaDownload, FaLaptopCode} from "react-icons/fa";
 import photo from "../../assets/illustration-home.webp";
-import { useEffect } from "react";
 import { getProjects } from "../../services/api";
 
 
@@ -10,10 +9,6 @@ const linkedinUrl = import.meta.env.VITE_LINKEDIN_URL;
 const email = import.meta.env.VITE_EMAIL;
 
 function Home() {
-  useEffect(() => {
-  getProjects().catch(() => {});
-}, []);
-
   return (
     <main className="home">
       <div className="home__noise"></div>
